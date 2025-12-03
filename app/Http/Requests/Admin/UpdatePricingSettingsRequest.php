0af63aa9eea2fd995bfc7d531af63eb3c1db02e7
Lukas <lukas@kotowicz.info>
2025-12-03 14:36:36 +0100

    Rechnungsstellung

--- app/Http/Requests/Admin/UpdatePricingSettingsRequest.php
+++ app/Http/Requests/Admin/UpdatePricingSettingsRequest.php
@@ -11,7 +11,7 @@ class UpdatePricingSettingsRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user() && $this->user()->isSuperAdmin();
+        return $this->user() && $this->user()->hasAnyRole(['super_admin', 'admin']);
     }
 
     /**
